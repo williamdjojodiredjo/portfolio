a0c902dcb152a6585dc10087d6351d26ae14d11f
William Djojodiredjo <127953429+WilliamDjojo1@users.noreply.github.com>
2024-08-20 02:29:49 +1000

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import TodoListPage from './components/TodoListPage/TodoListPage';
 import RandomQuoteMachinePage from './components/RandomQuoteMachinePage/RandomQuoteMachinePage';
